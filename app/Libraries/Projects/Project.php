de5ca0b488f3eefbb3fe9a6875524a5c257dc465
shenriquesdev <Unisergius@gmail.com>
2024-01-15 16:40:30 +0000

    [feature] Project class - new attributes, feature and image.
    Image will just be a jpg with the name being the slugged version of the projects name

--- app/Libraries/Projects/Project.php
+++ app/Libraries/Projects/Project.php
@@ -3,6 +3,8 @@
 namespace App\Libraries\Projects;
 
 use Illuminate\Support\Collection;
+use Illuminate\Support\Env;
+use Illuminate\Support\Str;
 
 class Project
 {
@@ -10,13 +12,29 @@ class Project
     public string $description;
     public Collection $links;
     public Collection $tags;
+    public bool $featured = false;
+    public string $image = '';
 
-    public function __construct(string $name, string $description, array $url, array $tags = [])
+    public function __construct(string $name, string $description, array $url, array $tags = [], bool $featured = false)
     {
         $this->name = $name;
         $this->description = $description;
         $this->links = collect([$url]);
         $this->tags = collect($tags);
+        $this->featured = $featured;
+
+        //check if image with slugged name exists in public/images/projects folder
+        $this->image = $this->getImageSource($name);
+    }
+
+    public function getImageSource(string $name): string
+    {
+        $image = Str::slug($name) . '.jpg';
+        $path = public_path('/img/projects/' . $image);
+        if (file_exists($path)) {
+            return $image;
+        }
+        return Env::get('DEFAULT_PROJECT_IMAGE', '/img/default.jpg');
     }
 
     public static function all(): array
