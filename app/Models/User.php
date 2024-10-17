e23c0cfe7fa109dde5118526cfa76dc05133f379
Pedro Pires (onelikeandidie) <onelikeandidie@proton.me>
2024-10-17 21:53:33 +0100

    Fix: Forgot to make user implement FilamentUser

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,8 +6,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Panel;
+use Filament\Models\Contracts\FilamentUser;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
