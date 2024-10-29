6db2d145ed050847bf5a4f58e8c47720ccd3c17a
Pedro Pires (onelikeandidie) <onelikeandidie@proton.me>
2024-10-29 15:10:35 +0000

    Adjusted: Featured session styles

--- resources/views/components/avatar.blade.php
+++ resources/views/components/avatar.blade.php
@@ -3,4 +3,5 @@
         <img class="w-8 h-8 rounded-full" src="{{ $host->image->url }}" alt="{{ $host->name }} profile picture">
     @endif
     <span>{{ $host->name }}</span>
+    {{ $slot }}
 </div>
