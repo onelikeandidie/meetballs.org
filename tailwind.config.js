68efee4277203d396ae46808f7d21f34f0af456f
Pedro Pires (onelikeandidie) <onelikeandidie@proton.me>
2025-03-07 16:06:15 +0000

    Revert the upgrade from tailwind 3 to 4
    This reverts most of commit 64ce2d6a0755369a3c64820a48c565d7ea6ed6fd.
    
    Filament 3 does not support tailwind 4 and it was
    being too difficult to work with on the backend.
    It would eventually be fixable but I don't have
    the time to fix it and was honestly not enjoying
    working on this anymore after that.

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter', 'Figtree', ...defaultTheme.fontFamily.sans],
+                display: ['Shrikhand', 'cursive'],
+            },
+        },
+    },
+    plugins: [
+        require('@tailwindcss/typography'),
+    ],
+};
